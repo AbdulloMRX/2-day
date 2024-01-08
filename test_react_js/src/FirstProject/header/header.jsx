78edd59d85d4e30a9b2eea6a631f74d392c2d758
AbdulloMRX <nuriddinovabdullo70@gmail.com>
2024-01-08 20:02:53 +0500

    Helllllo

---
+++ test_react_js/src/FirstProject/header/header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Styles, { HeaderContainer } from '../Style.js'
+
+
+
+export default  function Header() {
+  return (
+    <HeaderContainer>
+      
+<ul className='HEADER'>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>SERVICE</li>
+                <li>PORTFOLIO</li>
+                <li>PAGE</li>
+                <li>CONTACT</li>
+            </ul>
+
+
+    </HeaderContainer>
+    
+  
+  ) 
+
+}
